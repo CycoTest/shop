a85f77d40175862aeac628ffb136a539dfc51d31
CycoTest <aceldamadk@gmail.com>
2024-05-19 16:38:18 +0900

    Make Test
    1. index.html
    2. HomController : "/", "/about"

---
+++ src/main/java/com/personal/shop/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.personal.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String goHome() {
+
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    @ResponseBody
+    String goAbout() {
+
+        return "테스트 중이에요";
+    }
+}
