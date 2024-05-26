bd9cd2978e778353673ea2feb46bd7c9f34855e0
CycoTest <aceldamadk@gmail.com>
2024-05-26 15:59:39 +0900

    Divide the function in ItemController
    1. set business logic in ItemService
    2. bring the function of business logic in ItemService to itemController
    
    because this way prevents that the code get to be complicated and dirty.

--- src/main/java/com/personal/shop/controller/ItemController.java
+++ src/main/java/com/personal/shop/controller/ItemController.java
@@ -2,14 +2,15 @@ package com.personal.shop.controller;
 
 import com.personal.shop.entity.Item;
 import com.personal.shop.repository.ItemRepository;
+import com.personal.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -17,42 +18,40 @@ import java.util.Optional;
 public class ItemController {
 
     private final ItemRepository itemRepository;
+    private final ItemService itemService;
 
     @GetMapping("/list")
     String showList(Model model) {
-        List<Item> result = itemRepository.findAll();
-        System.out.println(result);
 
-        model.addAttribute("items", result);
+        model.addAttribute("items", itemService.bringItemList());
 
         return "list.html";
     }
 
-    @GetMapping("/write")
-    String showWriteForm() {
+    @GetMapping("/itemInfo/write")
+    String showItemWriteForm() {
 
         return "write.html";
     }
 
-    @PostMapping("/itemInfo")
+    @PostMapping("/itemInfo/write")
     String addItem(String title, Integer price) {
         // 파라미터에 @ModelAttribute Item item 으로 해도 됨
         // 대신 이렇게 할 경우, itemRepository.save(item); 만 쓰면 됨
-        Item item = new Item();
-        item.setTitle(title);
-        item.setPrice(price);
-
-        itemRepository.save(item);
+        // 하지만 함수 하나당 기능은 하나씩만 저장하는 게 낫기 때문에
+        // 비지니스 로직은 service 로 뺌
+        itemService.saveItem(title, price);
 
         return "redirect:/list";
     }
 
     @GetMapping("/detail/{id}")
     String viewItemDetail(@PathVariable Long id, Model model) {
-        Optional<Item> result = itemRepository.findById(id);
+
+        Optional<Item> result = itemService.bringItemById(id);
         if (result.isPresent()) {
             model.addAttribute("itemDetail", result.get());
-            System.out.println(result.get());
+//            System.out.println(result.get());
 
             return "detail.html";
         } else {
@@ -63,5 +62,5 @@ public class ItemController {
         // 에러 코드
         // 유저 잘못 = 4XX, 서버 잘못 = 5XX, 정상 작동 = 200
     }
-
+    
 }
