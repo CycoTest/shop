5e5a74671df6006b5647325b87c0e85b7b0bcc8f
CycoTest <aceldamadk@gmail.com>
2024-06-26 11:13:10 +0900

    Fix some navigation bar problem
    Since user, who already logged in, has clicked the Write menu on the navigation bar, the text thickness of Write has not change.
    
    But combine with id on div tag of Write, and make if statement of the id for double-check, the problem has fixed.

--- src/main/resources/static/js/navActive.js
+++ src/main/resources/static/js/navActive.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const currentPath = window.location.pathname;
     const navLinks = document.querySelectorAll('.nav-links a');
+    const writeLink = document.getElementById('write-link');
 
     navLinks.forEach(link => {
         const linkPath = link.getAttribute('href')
@@ -12,4 +13,27 @@ document.addEventListener('DOMContentLoaded', () => {
             link.classList.remove('active')
         }
     });
+
+    if (writeLink) {
+        writeLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const isAuthenticated = document.querySelector('.loginStatus span[sec\\:authorize="isAuthenticated()"]') != null;
+            if (isAuthenticated) {
+                // 로그인 상태일 때 유저가 현재 접속한 페이지에 따라 이동
+                if (currentPath.includes('/notice')) {
+                    window.location.href = '/noticeInfo/write';
+                } else {
+                    window.location.href = 'itemInfo/write';
+                }
+            } else {
+                // 로그인하지 않은 상태일 때
+                window.location.href = '/login?message=login_required';
+            }
+        });
+
+        // Write 페이지에서 Write 링크 활성화
+        if (currentPath.includes('/itemInfo/write') || currentPath.includes('/noticeInfo/write')) {
+            writeLink.classList.add('active');
+        }
+    }
 });
