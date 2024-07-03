ad7bb6cb671c85d5ac954f00c02fe916a30d6663
CycoTest <aceldamadk@gmail.com>
2024-07-03 18:07:06 +0900

    Set back-end process of my-page part 2
    2.  Rebuild the process of my page access
    remove the prototype of my page accessing, and split up the prototype progress.
    
    a. showMyPage
    This method handles main control to access my-page. It checks the value of "isVerified". This condition prevents that stranger tries to get into other's my-page page.
    
    If the value of "isVerified" is true, the method is aware of the owener's request. So, the method brings the user to their own my-page page.
    
    Also, the value of "isVerified" removes because this value is temporary condition.
    
    b. checkMyPage
    This method shows the re-Login form to user. Also, set the value of showSidebar to false because side bar sets only in main contents of my page part.
    
    c. verifyUser
    This method works the process of recheck that the user is really the owner of the account.

--- src/main/java/com/personal/shop/controller/MemberController.java
+++ src/main/java/com/personal/shop/controller/MemberController.java
@@ -5,10 +5,14 @@ import com.personal.shop.entity.CustomUser;
 import com.personal.shop.entity.Member;
 import com.personal.shop.repository.MemberRepository;
 import com.personal.shop.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -38,24 +42,56 @@ public class MemberController {
     }
 
     @GetMapping("/myPage")
-    String goMyPage(Authentication auth) {
-        CustomUser result = (CustomUser) auth.getPrincipal();
-        System.out.println(result.getDisplayName());
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+    String showMyPage(Model model, Authentication auth, HttpSession session) {
+        CustomUser user = (CustomUser) auth.getPrincipal();
+
+        // session에서 로그인 확인 상태 체크
+        Boolean isVerified = (Boolean) session.getAttribute("isVerified");
+        if (isVerified == null || !isVerified) {
+
+            return "redirect:/myPage/check";
+        }
+
+        model.addAttribute("user", user);
+        model.addAttribute("pageTitle", "마이 페이지");
+        model.addAttribute("showMyPageSideBar", true);
+
+        // 로그인 확인 상태 초기화
+        session.removeAttribute("isVerified");
 
         return "members/myPage";
     }
 
-    @GetMapping("/user/{id}")
-    @ResponseBody
-    public MemberDto getUser(@PathVariable Long id) {
-        Optional<Member> a = memberRepository.findById(id);
-        if (a.isPresent()) {
-            Member result = a.get();
+    @GetMapping("/myPage/check")
+    String checkMyPage(Authentication auth, Model model) {
+        CustomUser user = (CustomUser) auth.getPrincipal();
 
-            return new MemberDto(result.getUserName(), result.getDisplayName());
+        model.addAttribute("user", user);
+        model.addAttribute("pageTitle", "마이 페이지 로그인");
+        model.addAttribute("showSidebar", false);
+
+        return "members/myPageCheck";
+    }
+
+    @PostMapping("/myPage/verify")
+    String verifyUser(@RequestParam("password") String password,
+                      Authentication auth,
+                      HttpSession session,
+                      RedirectAttributes redirectAttributes) {
+        CustomUser user = (CustomUser) auth.getPrincipal();
+        if (memberService.verifyUser(user.getUsername(), password)) {
+            // 비밀번호가 일치, 유저 정보를 세션에 저장하고 myPage/info로 리다이렉트
+            session.setAttribute("isVerified", true);
+            redirectAttributes.addFlashAttribute("passedUser", user);
+
+            return "redirect:/myPage";
+        } else {
+            // 비밀번호 불일치, 에러 메세지와 다시 myPage로 리다이렉트
+            redirectAttributes.addFlashAttribute("error", "비밀번호가 일치하지 않습니다.");
+
+            return "redirect:/myPage/check";
         }
-
-        return null;
     }
 
 
